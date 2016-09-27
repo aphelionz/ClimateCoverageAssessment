fabb38d299483bf59cde856626d8cab9893ed5ad
Mark Henderson <henderson.mark@gmail.com>
2016-09-27 13:39:38 -0400

    More colors and longer graph

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
-  width = 960 - margin.left - margin.right,
+  width = 9600 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
 var formatDate = d3.timeFormat("%d-%b-%y");
